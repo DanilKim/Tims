cad9a3b90b2ab7ad8b721859bf3a71370324b561
yunseung hyun <yunseung_hyun@tmax.co.kr>
2022-06-02 17:51:13 +0900

    Screen

--- src/scene/screen.js
+++ src/scene/screen.js
@@ -1,16 +1,29 @@
 import {
-  Box
+  AppBar,
+  Box,
+  Toolbar,
+  Typography
 } from '@mui/material';
 
 import React from 'react';
 import * as THREE from 'three';
 import { observer } from 'mobx-react';
 import { Html } from '@react-three/drei';
+import MenuBtn from './menuBtn';
 
 function Screen() {
   return (
     <Html fullscreen>
-      <Box> hello world! </Box>
+      <Box sx={{ width: '100vw', height: '100vh' }}>
+        <AppBar position='static' sx={{ background: 'transparent', boxShadow: 'none' }}>
+          <Toolbar variant="dense">
+            <Typography component={'div'} sx={{ color: 'black', flexGrow: 1 }}>
+              Hello, YunSeung!
+            </Typography>
+            <MenuBtn />
+          </Toolbar>
+        </AppBar>
+      </Box>
     </Html>
   )
 }
