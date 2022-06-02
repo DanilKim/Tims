43973abe1ccdaa2dfcd73476f4fdcb944f5cb828
yunseung hyun <yunseung_hyun@tmax.co.kr>
2022-06-02 10:38:23 +0900

    remove contents

--- src/mainPage.js
+++ src/mainPage.js
@@ -5,14 +5,9 @@ import {
 import React from 'react';
 import { observer } from 'mobx-react';
 
-import TopBar from './components/TopBar/topBar';
-import Content from './components/content'
-
 function MainPage() {
   return(
     <Box sx={{ bgcolor: 'white', width: '100vw', height: '100vh' }}>
-      <TopBar />
-      <Content />
     </Box>
   );
 }
